6d949b416014a46349e4d86b2f5fc07ae5eb741d
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-06-15 09:00:53 -0700

    built project set up and added weather.js and css features

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Weather from './Weather';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <Weather defaultCity='New York' />
+
+        <footer>
+          This project was coded by{' '}
+          <a
+            href='https://wildbyte.org/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Angelica Victoria
+          </a>{' '}
+          and is{' '}
+          <a
+            href='https://github.com/angelicav1/weather-react-app'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
