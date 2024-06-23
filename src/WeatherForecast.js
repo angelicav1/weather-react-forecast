6c3dec434441883eb41eff30e79d738cf8d7036a
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-06-23 16:17:38 -0700

    updated app.css, weather.css, WeatherForecast.js, and WeatherInfo.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import WeatherIcon from './WeatherIcon';
 import "./WeatherForecast.css"
+import axios from 'axios';
 
-export default function WeatherForecast() {
+export default function WeatherForecast({coordinates}) {
+  const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    if (coordinates) {
+      const apiKey = 'f3009e4852fa0a079dab291dabf020c4';
+      const apiUrl = "https://api.openweathermpa.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric";
+
+      axios.get(apiUrl).then(handleResponse);
+    }
+  }, [coordinates]);
+  
+function handleResponse(response){
+    console.log(response.data);
+    setForecast(response.data);
+}
+
+if (forecast) {
   return (
     <div className='WeatherForecast'>
       <div className='row'>
         <div className='col'>
-          <div className='WeatherForecast-day'>Thu</div>
+          <div className='forecast-day'>Thu</div>
           <WeatherIcon code='01d' size={36} />
-          <div className='WeatherForecast-temperature'>
-            <span className='WeatherForecast-temperature-max'>19°</span>
-            <span className='WeatherForecast-temperature-min'>10°</span>
+          <div className='forecast-temp'>
+            <span className='forecast-temp-max'>{Math.round(forecast.daily[0].temp.max)}</span>
+            <span className='forecast-temp-min'>{Math.round(forecast.daily[0].temp.min)}</span>
           </div>
         </div>
       </div>
     </div>
   );
+}
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,6 +3,7 @@ import FormattedDate from './FormattedDate';
 import WeatherIcon from './WeatherIcon';
 import WeatherTemperature from './WeatherTemperature';
 
+
 export default function WeatherInfo(props) {
   return (
     <div className='WeatherInfo'>
@@ -15,12 +16,12 @@ export default function WeatherInfo(props) {
       </ul>
       <div className='row mt-3'>
         <div className='col-6'>
-          <div className='clearfix'>
-            <div className='float-left'>
+          <div className='d-flex'>
+            <div>
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
-            <div className='float-left'>
+            <div>
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
